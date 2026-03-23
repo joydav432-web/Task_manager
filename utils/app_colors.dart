becbdf91218a6d755562281324290842e1d58980
Joy dav <joydav432@gmail.com>
2026-03-23 11:46:46 +0600

    Add files via upload

---
+++ utils/app_colors.dart
@@ -0,0 +1,8 @@
+
+
+import 'package:flutter/animation.dart';
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static Color Pcolor = Colors.green;
+}
---
+++ utils/urls.dart
@@ -0,0 +1,5 @@
+class Urls{
+  static String _baseUrl = 'https://task.teamrabbil.com/api/v1';
+  static String signUpUrl = '$_baseUrl/registration';
+  static String logInUrl = '$_baseUrl/login';
+}
