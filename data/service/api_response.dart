f1186a4c2d72214aa91f4c8f2f1a06ef6addbc20
Joy dav <joydav432@gmail.com>
2026-03-23 11:50:32 +0600

    Add files via upload

---
+++ data/service/api_response.dart
@@ -0,0 +1,11 @@
+class ApiResponse{
+  final int responseCode;
+  final dynamic responseData;
+  final bool isSuccess;
+  final String? errorMsg;
+
+  ApiResponse({required this.responseCode,
+    required this.responseData,
+    required this.isSuccess,
+    this.errorMsg});
+}
